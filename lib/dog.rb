f5028f5b428efb5fceb522ddbcc79acd0b6f2f31
Vianka Rivera <viankalrivera@gmail.com>
2020-05-11 02:42:18 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,9 +2,9 @@ class Dog
   def initialize(dogs_name)
     @dogs_name = dogs_name
   end
-  def dogs_name
-    @dogs_name
+  def name
+    @name
   end
 end
 fido = Dog.new("Fido")
-fido.dogs_name
+fido.name
